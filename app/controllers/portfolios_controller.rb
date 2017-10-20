bafef997cb49c89999968571fd9fa7b358134bfc
“Andrew <andrewwoods88@gmail.com>
2017-10-20 12:02:22 -0400

    added destroy action to portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,6 +38,15 @@ class PortfoliosController < ApplicationController
     end
   end
 
+   def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+    end
+   end
+
 
   private
   
