8fa0942991be272eb9c0b3c38fabb8581b84156e
“Andrew <andrewwoods88@gmail.com>
2017-10-21 17:09:23 -0400

    setuo routes for about-me and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  
+  
+ 
+  #get 'pages/about'
+  get 'about-me', to: 'pages#about'
+
+  #get 'pages/contact'
+  get 'contact', to: 'pages#contact'
+
   resources :portfolios
+  resources :blogs
+  
   root to: 'pages#home'
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
