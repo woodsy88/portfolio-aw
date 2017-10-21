ee70bd7a0bf66e135b9483451b22de1d19690b28
“Andrew <andrewwoods88@gmail.com>
2017-10-21 17:25:09 -0400

    cutomized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   #get 'pages/contact'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+                                              #as: adds the prefix portfolio_show_path
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
   
   root to: 'pages#home'
