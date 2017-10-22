2f9d9968b3b06ada5e4f94ab8371d9cff13b28bf
“Andrew <andrewwoods88@gmail.com>
2017-10-22 10:20:27 -0400

    Added toggle_status to blog controller
    - toggle status now working in view
    - blogs can be toggled from published to draft and back again

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
                                               #as: adds the prefix portfolio_show_path
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
   root to: 'pages#home'
 
