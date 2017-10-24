70e3788764d911f7d8f0b5b4b5c977f10b6b977d
“Andrew <andrewwoods88@gmail.com>
2017-10-24 11:08:36 -0400

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,20 @@ class Portfolio < ApplicationRecord
   end
 
   scope :rub_on_rails_portfolio, -> { where(subtitle: 'Ruby on Rails') }
+
+  #callback - after a portfolio item has been initialized (when new action called)
+  # a process that automatically runs after going to new index
+  after_initialize :set_defaults
+  #sets defaults for new portfolio attributes
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    #the above is essentially doing the below if statement in one line
+    # if self.main_image == nil
+          #  self.main_image = "http://via.placeholder.com/600x400"
+      # end
+    self.thumb_image ||= "http://via.placeholder.com/300x150"
+  end
 end
+
+
+          
