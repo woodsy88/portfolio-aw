2a60893ccacda0d98e412dfa1407be37b1998b87
“Andrew <andrewwoods88@gmail.com>
2017-10-24 14:44:16 -0400

    Implemented concern for adding placeholder images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,5 @@
 class Portfolio < ApplicationRecord
-
+include Placeholder
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -13,13 +13,14 @@ class Portfolio < ApplicationRecord
   # a process that automatically runs after going to new index
   after_initialize :set_defaults
   #sets defaults for new portfolio attributes
+  # if an image is entered by the user, then it skips these, if no image is added,then it sets defaults
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     #the above is essentially doing the below if statement in one line
     # if self.main_image == nil
           #  self.main_image = "http://via.placeholder.com/600x400"
       # end
-    self.thumb_image ||= "http://via.placeholder.com/300x150"
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
 
