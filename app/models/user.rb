0087354204c5418d3b438a48e6c5e15a89cf5854
“Andrew <andrewwoods88@gmail.com>
2017-11-01 12:33:20 -0400

    Completed virtual attributes for first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+# Virtual Atrribute for first_name and last_name
+  validates_presence_of :name
+
+  def first_name
+    #runs on each instance
+    self.name.split.first
+  end
+
+  def last_name
+      self.name.split.last
+  end
 end
