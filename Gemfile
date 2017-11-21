545314b7415bcc8494d3c594104fdf48eb7e6b8b
“Andrew <andrewwoods88@gmail.com>
2017-11-20 23:43:38 -0500

    Installed jquery-ui gem/imported jquery-ui files, and created html.sortable javascript

--- Gemfile
+++ Gemfile
@@ -71,4 +71,4 @@ gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
 
 gem 'kaminari', '~> 1.1', '>= 1.1.1'
 
-
+gem 'jquery-ui-rails', '~> 6.0', '>= 6.0.1'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require popper
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui
+//= require popper
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
