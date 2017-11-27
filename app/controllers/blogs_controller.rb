b1c7aecec31d67e5c89400a33962c86c6f5acb33
“Andrew <andrewwoods88@gmail.com>
2017-11-26 21:58:44 -0500

    Built Form For - blogs. Updated blog.rb to make topic association optional

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -75,8 +75,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, 
-                                   :body
-                                   )
+      params.require(:blog).permit(:title, :body)
     end
 end
