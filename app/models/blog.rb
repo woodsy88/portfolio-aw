ef4484747c4b0d2a9b5374d3d1ff5bc7b36e6bbc
“Andrew <andrewwoods88@gmail.com>
2017-12-02 19:57:42 -0500

    Ordered newest blogs to appear first on index list when created

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,6 +5,9 @@ class Blog < ApplicationRecord
   #takes the title of the blog, and converts it into the slug
   friendly_id :title, use: :slugged
 
+  #part of paginate, latest blogs created are the first to show
+  default_scope -> { order(updated_at: :desc) }
+
   validates_presence_of :title, :body
 
   belongs_to :topic, optional: true
