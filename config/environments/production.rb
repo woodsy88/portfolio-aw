2598a18ea49f52d9de8b5fc85dc2576b9c6de581
“Andrew <andrewwoods88@gmail.com>
2017-12-09 11:52:14 -0500

    Integrated production.rb calls for ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://andrew-w-portfolio.herokuapp.com', 'http://andrew-w-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://andrew-w-portfolio.herokuapp.com"
 end
