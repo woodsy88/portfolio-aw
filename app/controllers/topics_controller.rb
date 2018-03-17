2968b0189c8610b765df1dd61288b8f4e9cac3a9
“Andrew <andrewwoods88@gmail.com>
2018-03-17 14:14:37 -0400

    Added new color palet, changed nav color, added hompeage banner

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
 before_action :set_sidebar_topics
+before_action :set_topic, only: [:edit, :show, :update, :destroy]
   layout 'blog'
 
  def new 
@@ -22,7 +23,6 @@ before_action :set_sidebar_topics
   end
 
   def show
-    @topic = Topic.find(params[:id])
     
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(5)
@@ -31,14 +31,35 @@ before_action :set_sidebar_topics
     end
   end
 
+
+  def edit
+  end
+
+  def update
+    
+    respond_to do |format|
+      if @topic.update(topic_params)
+         format.html { redirect_to (topics_path) }
+        else
+          format.html { render :edit }
+      end  
+    end
+  end
+
+
+
   private
   #uses method from topic.rb
   def set_sidebar_topics
     @side_bar_topics = Topic.with_blogs
   end
 
-    def topic_params
+  def topic_params
     params.require(:topic).permit(:title)
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
   end
 
 end
