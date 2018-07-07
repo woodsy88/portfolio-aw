f2f3ed181a047e465ccfabdfe5e99a4654d37133
“Andrew <andrewwoods88@gmail.com>
2018-07-07 17:20:23 -0400

    Updated style sheets, organized style sheets, stylized homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,16 @@
 class PagesController < ApplicationController
   def home
     @post = Blog.all
+    @recent_snippets = Blog.where(status: "published").order("created_at DESC").limit(3)
   end
 
   def about
     @skills = Skill.all
     @jobs = Job.all
+  end
+
+  def recent_snippets
+    @recent_snippets = Blog.order("created_at DESC").limit(3)
   end
 
   def contact
