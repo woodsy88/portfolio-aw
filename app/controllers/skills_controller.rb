d21de1a7557d4d6dda25e1e825e9e5388b18c5c4
“Andrew <andrewwoods88@gmail.com>
2018-07-11 18:38:44 -0400

    created skills controller, resources and views

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,46 @@
+class SkillsController < ApplicationController
+  before_action :set_skill, only: [:show, :edit, :update]
+  def new
+     @skill = Skill.new
+  end
+  
+  def create
+		@skill = Skill.new(skill_params)
+	#	@skill.user_id = current_user.id
+
+		if @skill.save
+			redirect_to @skill, notice: 'Your skill was created successfully'
+		else
+			render :new
+		end    
+    
+  end
+  
+  def edit
+  end
+  
+  def update
+        @skill.update(skill_params)
+        redirect_to @skill, notice: 'Your skill was created successfully'    
+  end
+
+  def show
+     @skill_jobs = @skill.jobs
+  end
+  
+  def index
+    @skills = Skill.all
+  end
+
+  private
+
+
+  def set_skill
+    @skill = Skill.find(params[:id])
+  end
+
+    def skill_params
+        params.require(:skill).permit(:title, :percent_utilized, :badge)   
+    end   
+  
+end
