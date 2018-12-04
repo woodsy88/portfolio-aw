f79d2d0e46b00873aa5d787ccbe3720201d602cb
“Andrew <andrewwoods88@gmail.com>
2018-12-04 13:13:30 -0500

    refactored jobs into react component

--- app/javascript/components/Job.js
+++ app/javascript/components/Job.js
@@ -6,12 +6,13 @@ function JobSkill (props) {
     <span className="skill-item">{props.title}</span>
   )
 }
-
 class Job extends React.Component {
   render () {
     return (
       <React.Fragment>
          <h2 className="sub-hero-text">{this.props.company}</h2>
+         <h3>{this.props.title}</h3>
+         <p>{this.props.date_text}</p>
          <p>{this.props.body}</p>
          <div>{this.props.job_skills.map(function(skill){
             return <JobSkill title={skill.title} />;
@@ -23,10 +24,11 @@ class Job extends React.Component {
 
 Job.propTypes = {
   company: PropTypes.string,
-  // jobTitle: PropTypes.string,
+  title: PropTypes.string,
   body: PropTypes.string,
-  // position: PropTypes.string,
-  // date_text: PropTypes.string,
+  position: PropTypes.string,
+  date_text: PropTypes.string,
   job_skills: PropTypes.array
 };
+
 export default Job
