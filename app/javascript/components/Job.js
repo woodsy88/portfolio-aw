f9d10391eab9bac05ca6aa800aadb74569fd6366
“Andrew <andrewwoods88@gmail.com>
2018-12-05 16:21:13 -0500

    passing the job.skills list as an array of object and pulling out the title and id

--- app/javascript/components/Job.js
+++ app/javascript/components/Job.js
@@ -1,21 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-function JobSkill (props) {
-  return (
-    <span className="skill-item">{props.title}</span>
-  )
-}
 class Job extends React.Component {
   render () {
+
+    const { job_skills } = this.props;
+
     return (
       <React.Fragment>
          <h2 className="sub-hero-text">{this.props.company}</h2>
          <h3>{this.props.title}</h3>
          <p>{this.props.date_text}</p>
          <p>{this.props.body}</p>
-         <div>{this.props.job_skills.map(function(skill){
-            return <JobSkill title={skill.title} />;
+
+        {/* check what data is coming in from job_skills */}
+         {console.log(job_skills)}   
+         <div>{job_skills.map(function({title, id}){
+              console.log('title: ', title, 'id: ', id);
+            return (<span className="skill-item"><a href={"skills/" + id}>{title}</a></span>)
          })}</div>
       </React.Fragment>
     );
