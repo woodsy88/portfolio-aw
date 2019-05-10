2f4d577371714a3608619b07ad0e7809b88d61a8
“Andrew <andrewwoods88@gmail.com>
2019-05-10 19:24:17 -0400

    implemented React timeline feature

---
+++ app/javascript/TimeLineData.js
@@ -0,0 +1,27 @@
+const timelineData = [{
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url: 'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#000'
+    },
+    link: {
+      url: 'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  }
+];
+
+export default timelineData;
