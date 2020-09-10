6e0342ab95bb4b7dd6f47286c56be17fe8b82f39
“Andrew <andrewwoods88@gmail.com>
2020-09-10 18:23:42 -0400

    remove tag file migration

--- db/migrate/20190827174209_remove_tags_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTagsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :taggings
-  end
-end
--- db/migrate/20190827174504_remove_taggings_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTaggingsTable < ActiveRecord::Migration[6.0]
-  def change
-       drop_table :tags
-  end
-end
